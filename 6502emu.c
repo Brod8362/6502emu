65741657a558774fed7bbc6fd36ca316e1f61f6c
Brod8362 <brod8362@gmail.com>
2021-11-17 17:11:01 -0600

    Add proper clockspeed emulation

--- 6502emu.c
+++ 6502emu.c
@@ -3,11 +3,12 @@
 #include "instr_map.h"
 #include "types.h"
 
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 
 /*
-Read value in memory stored at current program counter, and increment program counter
+Read 8 bit value in memory stored at current program counter, and increment program counter
 */
 uint8_t read_8(emustate* emu) {
     uint8_t v = emu->memory[emu->pc/256][emu->pc%256];
@@ -15,6 +16,9 @@ uint8_t read_8(emustate* emu) {
     return v;
 }
 
+/*
+Read 16 bit value in memory stored at current program counter and PC+1, and increment program counter 
+*/
 uint16_t read_16(emustate* emu) {
     return read_8(emu) | (read_8(emu) << 8);
 }
@@ -37,25 +41,24 @@ int main(int argc, char** argv) {
     
     emustate emu;
     reset_proc(&emu);
-    int clock = 1000000; //1Mhz
+    int clockspeed = 1000000; //1Mhz
     abs_t adr = 0x4000;
-    size_t program_size = 0;
     uint8_t byte;
     while (read(STDIN_FILENO, &byte, 1) > 0) { //read program from stdin , starting at mem address 0x4000
         emu.memory[adr/256][adr%256] = byte;
-        program_size++;
+        adr++;
     }
-    printf("Read %lu program bytes into memory\n", program_size);
+    printf("Read %d program bytes into memory\n", adr-0x4000);
 
-    emu.pc = 0x4000; //set program counter to 0x4000
+    emu.pc = 0x4000; //set program counter to beginning of program in memory
     while (1) {
         uint8_t opcode = read_8(&emu);
         const instr_info* i = instr_map[opcode];
         if (i == NULL) {
-            //invalid opcode
+            printf("Invalid opcode $%02x\n @ $%04x\n", opcode, emu.pc-1);
             return 1;
         }
-        printf("Decoded instruction %s ($%x) @ $%4x\n(emu->pc++)", i->name, opcode, emu.pc);
+        printf("Decoded instruction %s ($%02x) @ $%04x\n", i->name, opcode, emu.pc-1);
         cycles_t cycles;
         switch (i->type) {
             case Implied:
@@ -81,7 +84,11 @@ int main(int argc, char** argv) {
                 printf("Failed to determine instruction type");
                 return 2;
         }
+        
         //TODO sleep with cycles
+        int sleep_time = ((float)cycles/clockspeed)*1000000; //determine time for cycles to occur, convert to microseconds
+        printf("%s ($%02x) took %d cycles to execute\n", i->name, opcode, cycles);
+        usleep(sleep_time);
     }
 
     return 0;
